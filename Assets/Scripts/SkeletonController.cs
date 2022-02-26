247382785143fb560f063c74d18690df76a21988
profotoce <romain.vanhee@gmail.com>
2022-02-26 16:24:20 +0100

    Update SkeletonController.cs
    rotation of skeleton toward player

--- Assets/Scripts/SkeletonController.cs
+++ Assets/Scripts/SkeletonController.cs
@@ -12,6 +12,7 @@ public class SkeletonController : MonoBehaviour
     private int health;
     private float rangeAttack = 15f;
     private float rangeTargetPlayer = 50f;
+    private float speed = 0.1f;
 
     void Start()
     {
@@ -29,7 +30,7 @@ public class SkeletonController : MonoBehaviour
         Vector3 persoPos = notrePerso.transform.position;
         float dist = (persoPos- transform.position).sqrMagnitude;
         dist = Mathf.Sqrt(dist);
-        Debug.Log("calculeDistance" + dist + "alaed ");
+        //Debug.Log("calculeDistance" + dist + "alaed ");
         return dist;
     }
     IEnumerator setAnimator()
@@ -38,9 +39,24 @@ public class SkeletonController : MonoBehaviour
         {
             animator.SetBool("isDead", this.health <= 0);
             animator.SetBool("canAttack", calculeDistance() < rangeAttack);
-            animator.SetBool("playerDetected", calculeDistance() < rangeTargetPlayer);
-            yield return new WaitForSeconds(.3f);
+            if (calculeDistance() < rangeTargetPlayer)
+            {
+                if (!animator.GetBool("canAttack"))
+                {
+
+                    move();
+                }
+                animator.SetBool("playerDetected",true);
+            }
+            yield return new WaitForSeconds(.8f);
         }
 
+    }
+    private void move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, notrePerso.transform.position, speed * Time.deltaTime);
+        Vector3 direction = notrePerso.transform.position - transform.position;
+        direction.Normalize();
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.1f * Time.deltaTime);
     }
 }
